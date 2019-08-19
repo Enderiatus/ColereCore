7d006d5ede36f8d8606425e757f81453abf28506
Colere <Colere@DESKTOP-1LRTNNV>
2019-08-19 22:11:08 +0300

    Farmer custom items added

---
+++ ColereCore/src/me/Enderiatus/ColereCore/Jobs/Events/FarmerFarmEvent.java
@@ -0,0 +1,36 @@
+package me.Enderiatus.ColereCore.Jobs.Events;
+
+import java.util.Random;
+
+import org.bukkit.block.data.Ageable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import me.Enderiatus.ColereCore.Main;
+import me.Enderiatus.ColereCore.Items.CustomItemManager;
+import me.Enderiatus.ColereCore.Jobs.Jobs;
+import me.Enderiatus.ColereCore.Status.StatusManager;
+
+public class FarmerFarmEvent implements Listener{
+	
+	Main plugin;
+	public FarmerFarmEvent(Main plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void cropBreak(BlockBreakEvent e) {
+		if(e.getBlock().getBlockData() instanceof Ageable && StatusManager.PLAYER_STATUS.get(e.getPlayer()).getPlayerJob() == Jobs.FARMER) {
+			Ageable age = (Ageable) e.getBlock().getBlockData();
+			if(age.getAge() != age.getMaximumAge()) 
+				return;
+			Jobs.addJobLevelXP(e.getPlayer(), 8);
+			int chanceOfItem = new Random().nextInt(1000)+1;
+			if(chanceOfItem > StatusManager.PLAYER_STATUS.get(e.getPlayer()).getJobsLevel()) 
+				return;
+			CustomItemManager.dropCustomItem("Farmer", e.getPlayer());
+		}
+	}
+
+}
