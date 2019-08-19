446f2338afa49557e3bd3e5d366152eb0951384f
Colere <Colere@DESKTOP-1LRTNNV>
2019-08-19 22:33:27 +0300

    Added fisher drops

--- ColereCore/src/me/Enderiatus/ColereCore/Jobs/Events/FisherFishEvent.java
+++ ColereCore/src/me/Enderiatus/ColereCore/Jobs/Events/FisherFishEvent.java
@@ -1,10 +1,16 @@
 package me.Enderiatus.ColereCore.Jobs.Events;
 
+import java.util.Random;
+
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 
 import me.Enderiatus.ColereCore.Main;
+import me.Enderiatus.ColereCore.Items.CustomItemManager;
+import me.Enderiatus.ColereCore.Jobs.Jobs;
+import me.Enderiatus.ColereCore.Status.StatusManager;
 
 public class FisherFishEvent implements Listener {
 	
@@ -15,7 +21,14 @@ public class FisherFishEvent implements Listener {
 	
 	@EventHandler
 	public void catchFish(PlayerFishEvent e) {
-		
+		if(!(StatusManager.PLAYER_STATUS.get(e.getPlayer()).getPlayerJob() == Jobs.FISHER)) 
+			return;
+		if(!(e.getCaught() instanceof Item)) 
+			return;
+		int randomChance = new Random().nextInt(1000)+1;
+		if(randomChance <= StatusManager.PLAYER_STATUS.get(e.getPlayer()).getJobsLevel()) {
+			CustomItemManager.dropCustomItem("Fisher", e.getPlayer());
+		}
 	}
 
 }
